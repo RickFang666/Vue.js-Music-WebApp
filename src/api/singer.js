b60cac7092ab728755aad8c24f210b18f0c11f97
RickFang666 <rickfang0123@gmail.com>
2017-07-23 19:12:00 +0800

    update SingerList

---
+++ src/api/singer.js
@@ -0,0 +1,19 @@
+import jsonp from 'common/js/jsonp'
+import {commonParams, options} from './config'
+
+export function getSingerList() {
+  const url = 'https://c.y.qq.com/v8/fcg-bin/v8.fcg'
+  const data = Object.assign({}, commonParams, {
+    channel: 'singer',
+    page: 'list',
+    key: 'all_all_all',
+    pagesize: 100,
+    pagenum: 1,
+    gostUin: 0,
+    needNewCode: 0,
+    platform: 'yqq',
+    g_tk: 1664029744
+  })
+
+return jsonp(url, data, options)
+}
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -3,7 +3,76 @@
 </template>
 
 <script type="text/ecmascript-6">
+import {getSingerList} from 'api/singer'
+import {ERR_OK} from 'api/config'
+import Singer from 'common/js/singer'
 
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 30
+
+export default {
+  data() {
+    return {
+      singers:[]
+    }
+  },
+  created() {
+    this._getSingerList()
+  },
+  methods: {
+    _getSingerList(){
+      getSingerList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.singers = res.data.list
+          console.log(this._normalizeSinger(this.singers))
+        }
+      })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+      list.forEach((item,index) =>{
+        if(index < HOT_SINGER_LEN) {
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]) {
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+      // 处理map，得到有序列表
+      let hot = []
+      let ret = []
+      for(let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)) {
+          ret.push(val)
+        }else if (val.title === HOT_NAME) {
+          hot.push(val)
+        }
+      }
+      ret.sort((a,b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      console.log(map)
+      return hot.concat(ret)
+    }
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
