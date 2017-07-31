a6e7c08b47295803777338dbb591d7fc243a33e4
RickFang666 <rickfang0123@gmail.com>
2017-07-31 23:56:33 +0800

    set vuex

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -32,4 +32,22 @@ let vendor = (() => {
     ms: 'msTransform',
     standard: 'transform'
   }
+  for (let key in transformNames) {
+    if(elementStyle[transformNames[key]]!== undefined) {
+      return key
+    }
+  }
+  return false
 })()
+
+export function  prefixStyle(style){
+  if (vendor === false) {
+    return false
+  }
+  if (vendor === 'standard') {
+    return style
+  }
+
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
+
--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -1,10 +1,16 @@
 <template>
 <div class="music-list">
-  <div class="back">
+  <div @click="back" class="back">
     <i class="icon-back"></i>
   </div>
   <h1 class="title" v-html="title"></h1>
   <div class="bg-image" :style="bgStyle" ref="bgImage">
+    <div class="play-wrapper">
+        <div ref="playBtn" v-show="songs.length>0" class="play">
+          <i class="icon-play"></i>
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
     <div class="filter" ref="filter"></div>
   </div>
   <div class="bg-layer" ref="layer"></div>
@@ -15,6 +21,9 @@
     <div class="song-list-wrapper">
       <song-list :songs="songs"></song-list>
     </div>
+    <div v-show="!songs.length" class="loading-container">
+      <loading></loading>
+    </div>
   </scroll>
 </div>
 </template>
@@ -22,8 +31,12 @@
 <script type="text/ecmascript-6">
 import Scroll from 'base/scroll/scroll'
 import SongList from 'base/song-list/song-list'
+import {prefixStyle} from 'common/js/dom'
+import Loading from 'base/loading/loading'
 
 const RESERVED_HEIGHT = 40
+const transform = prefixStyle('transform')
+const backdrop = prefixStyle('backdrop-filter')
 
 export default {
   props: {
@@ -65,13 +78,17 @@ export default {
       }
     },
     methods: {
+      back() {
+        this.$router.back()
+      },
       scroll(pos) {
         this.scrollY = pos.y
       },
     },
     components: {
       Scroll,
-      SongList
+      SongList,
+      Loading
     },
     watch: {
       scrollY(newY) {
@@ -79,8 +96,7 @@ export default {
         let scale = 1
         let blur = 0
         let translateY = Math.max(this.minTranslateY, newY)
-        this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
-        this.$refs.layer.style['-webkit-transform'] = `translate3d(0,${translateY}px,0)`
+        this.$refs.layer.style[transform] = `translate3d(0,${translateY}px,0)`
         const percent = Math.abs(newY / this.imageHeight)
         if (newY > 0) {
           scale = 1 + percent
@@ -88,13 +104,15 @@ export default {
         } else {
           blur = Math.min(20 * percent, 20)
         }
-        this.$refs.filter.style['nackdrop-filter'] =`blur($(blur)px)`
+        this.$refs.filter.style[backdrop] =`blur($(blur)px)`
         if(newY<this.minTranslateY) {
           zIndex = 10
           this.$refs.bgImage.style.paddingTop = `${RESERVED_HEIGHT}px`
+          this.$refs.playBtn.style.display = 'none'
         }else {
           this.$refs.bgImage.style.paddingTop = '70%'
           this.$refs.bgImage.style.zIndex = 0
+          this.$refs.playBtn.style.display = 'block'
         }
         this.$refs.bgImage.style.zIndex = zIndex
         this.$refs.bgImage.style['transform'] = `scale(${scale})`
