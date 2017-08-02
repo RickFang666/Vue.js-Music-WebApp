ba72100f76afb59c9a9619a06191b192338e270a
RickFang666 <rickfang0123@gmail.com>
2017-08-02 21:42:35 +0800

    player bar

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="player" v-show="playlist.length>0">
-    <transition name="normal">
+    <transition name="normal"
+                @enter="enter"
+                @after-enter="afterEnter"
+                @leave="leave"
+                @after-leave="afterLeave">
     <div class="normal-player" v-show="fullScreen">
       <div class="background">
         <img width="100%" height="100%" :src="currentSong.image">
@@ -14,25 +18,33 @@
       </div>
       <div class="middle">
         <div class="middle-l">
-          <div class="cd-wrapper">
-            <div class="cd">
+          <div class="cd-wrapper" ref="cdWrapper">
+            <div class="cd" :class="cdCls">
               <img class="image" :src="currentSong.image">
             </div>
           </div>
         </div>
       </div>
       <div class="bottom">
+        <div class="progress-wrapper">
+          <span class="time time-l">{{format(currentTime)}}</span>
+          <div class="progress-bar-wrapper">
+            <progress-bar :percent="percent"></progress-bar>
+          </div>
+          <span class="time time-r">{{format(currentSong.duration)}}</span>
+        </div>
         <div class="operators">
+
           <div class="icon i-left">
             <i class="icon-sequence"></i>
           </div>
-          <div class="icon i-left">
+          <div :class="disableCls" @click="prev" class="icon i-left">
             <i class="icon-prev"></i>
           </div>
-          <div class="icon i-center">
-            <i class="icon-play"></i>
+          <div :class="disableCls" @click="togglePlaying" class="icon i-center">
+            <i :class="playIcon"></i>
           </div>
-          <div class="icon i-right">
+          <div :class="disableCls" @click="next" class="icon i-right">
             <i class="icon-next"></i>
           </div>
           <div class="icon i-right">
@@ -45,29 +57,59 @@
     <transition name="mini">
     <div class="mini-player" v-show="!fullScreen" @click="open" >
       <div class="icon">
-        <img width="40" height="40" :src="currentSong.image">
+        <img :class="cdCls" width="40" height="40" :src="currentSong.image">
       </div>
       <div class="text">
         <h2 class="name" v-html="currentSong.name"></h2>
         <p class="desc" v-html="currentSong.singer"></p>
       </div>
       <div class="control">
-        <i class="icon-playlist"></i>
+          <i @click.stop="togglePlaying":class="miniIcon"></i>
+      </div>
+      <div class="control">
+        <i  class="icon-playlist"></i>
       </div>
     </div>
     </transition>
+    <audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error" @timeupdate="updateTime"></audio>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import {mapGetters, mapMutations} from 'vuex'
+import {prefixStyle} from 'common/js/dom'
+import animations from 'create-keyframe-animation'
+import ProgressBar from 'base/progress-bar/progress-bar'
+
+const transform = prefixStyle('transform')
+const transitionDuration = prefixStyle('transitionDuration')
 
 export default {
+  data() {
+    return {
+      songReady: false,
+      currentTime: 0
+    }
+  },
   computed: {
+    disableCls() {
+      return this.songReady? '':'disable'
+    },
+    cdCls() {
+      return this.playing? 'play': 'pause'
+    },
+    playIcon() {
+      return this.playing? 'icon-pause':'icon-play'
+    },
+    miniIcon() {
+      return this.playing? 'icon-pause-mini':'icon-play-mini'
+    },
     ...mapGetters([
       'fullScreen',
       'playlist',
       'currentSong',
+      'currentIndex',
+      'playing',
       'currentIndex'
     ])
   },
@@ -82,8 +124,143 @@ export default {
       this.setFullScreen(true)
     },
     ...mapMutations({
-      setFullScreen: 'SET_FULL_SCREEN'
-    })
+      setFullScreen: 'SET_FULL_SCREEN',
+      setPlayingState: 'SET_PLAYING_STATE',
+      setCurrentIndex: 'SET_CURRENT_INDEX'
+    }),
+    enter(el, done) {
+      const{x, y, scale}= this._getPosAndScale()
+
+      let animation = {
+        0: {
+          transform: `translate3d(${x}px,${y}px,0) scale(${scale})`
+        },
+        60: {
+          transform: `translate3d(0,0,0) scale(1.1)`
+        },
+        100: {
+          transform: `translate3d(0,0,0) scale(1)`
+        }
+      }
+
+      animations.registerAnimation({
+        name: 'move',
+        animation,
+        presets: {
+          duration: 400,
+          easing: 'linear'
+        }
+      })
+
+      animations.runAnimation(this.$refs.cdWrapper, 'move', done)
+      },
+    afterEnter() {
+      animations.unregisterAnimation('move')
+      this.$refs.cdWrapper.style.animation = ''
+    },
+    leave(el, done) {
+      this.$refs.cdWrapper.style.transition = 'all 0.4s'
+      const {x, y, scale} = this._getPosAndScale()
+      this.$refs.cdWrapper.style[transform] = `translate3d(${x}px,${y}px,0) scale(${scale})`
+      this.$refs.cdWrapper.addEventListener('transitionend', done)
+    },
+    afterLeave() {
+      this.$refs.cdWrapper.style.transition = ''
+      this.$refs.cdWrapper.style[transform] = ''
+    },
+    _getPosAndScale() {
+      //miniPlayer width
+      const targetWidth = 40
+      //miniPlayer padding left
+      const paddingLeft = 40
+      //miniPlayer paddingBottom
+      const paddingBottom = 30
+      //player paddingTop
+      const paddingTop = 80
+      //player width
+      const width = window.innerWidth * 0.8
+      const scale = targetWidth/ width
+      const x = -(window.innerWidth/2 - paddingLeft)
+      const y = window.innerHeight - paddingTop - width/2 - paddingTop
+      return {
+        x,
+        y,
+        scale
+      }
+    },
+    format(interval) {
+      interval = interval | 0
+      const minute = this._pad(interval / 60 | 0)
+      const second = this._pad(interval % 60 | 0)
+      return  `${minute}:${second}`
+    },
+    _pad(num, n = 2) {
+      let len = num.toString().length
+      while(len < n) {
+        num = '0' + num
+        len++
+      }
+      return num
+    },
+    updateTime(e) {
+      this.currentTime = e.target.currentTime
+    },
+    next() {
+      if(!this.songReady) {
+        return
+      }
+      let index = this.currentIndex + 1
+      if(index === this.playlist.length) {
+        index = 0
+      }
+      this.setCurrentIndex(index)
+      if(!this.playing) {
+        this.togglePlaying()
+      }
+      this.songReady = false
+    },
+    prev() {
+      if(!this.songReady) {
+        return
+      }
+      let index = this.currentIndex - 1
+      if(index === -1) {
+        index = this.playlist.length - 1
+      }
+      this.setCurrentIndex(index)
+      if(!this.playing) {
+        this.togglePlaying()
+      }
+      this.songReady = false
+    },
+    ready() {
+      this.songReady = true
+    },
+    error() {
+      this.songReady = true
+    },
+    togglePlaying() {
+      this.setPlayingState(!this.playing)
+    },
+    percent() {
+      return this.currentTime / this.currengSong.duration
+    },
+  },
+  watch: {
+    currentSong() {
+      this.$nextTick(() => {
+        this.$refs.audio.play()
+      })
+    },
+    playing(newPlaying) {
+      const audio = this.$refs.audio
+      this.$nextTick(() => {
+        newPlaying? audio.play() : audio.pause()
+      })
+    }
+  },
+  components: {
+    ProgressBar,
   }
 }
 </script>
@@ -235,8 +412,10 @@ export default {
             width: 30px
             &.time-l
               text-align: left
+              padding-right: 5px
             &.time-r
               text-align: right
+              padding-Left: 5px
           .progress-bar-wrapper
             flex: 1
         .operators
